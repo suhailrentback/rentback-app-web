d4ac1e84db68d5d155c72ff357cee32a4babb181
suhailrentback <suhail@rentback.app>
2025-10-09 20:49:31 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#ecfdf5',
+          100: '#d1fae5',
+          200: '#a7f3d0',
+          300: '#6ee7b7',
+          400: '#34d399',
+          500: '#10b981',
+          600: '#059669',
+          700: '#047857',
+          800: '#065f46',
+          900: '#064e3b'
+        }
+      }
+    }
+  },
+  plugins: [],
+};
+
+export default config;
