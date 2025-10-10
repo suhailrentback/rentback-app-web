d1e26f765ec06cdf01c590eb5ed06e4533e628de
suhailrentback <suhail@rentback.app>
2025-10-10 13:54:49 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,16 @@
 import type { Config } from 'tailwindcss';
 
-const config: Config = {
+export default {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        brand: {
-          50: '#ecfdf5',
-          100: '#d1fae5',
-          200: '#a7f3d0',
-          300: '#6ee7b7',
-          400: '#34d399',
-          500: '#10b981',
+        emerald: {
           600: '#059669',
-          700: '#047857',
-          800: '#065f46',
-          900: '#064e3b'
-        }
-      }
-    }
+          400: '#34d399',
+        },
+      },
+    },
   },
   plugins: [],
-};
-
-export default config;
+} satisfies Config;
