01549f301da3d0ec9a18bc9f0e072446ab5d9d11
suhailrentback <suhail@rentback.app>
2025-10-11 14:31:52 +0200

    Create route.ts

---
+++ app/sign-out/route.ts
@@ -0,0 +1,10 @@
+// app/sign-out/route.ts
+import { NextResponse } from 'next/server';
+import { supabaseServer } from '../../lib/supabase/server';
+
+export async function GET(req: Request) {
+  const supabase = supabaseServer();
+  await supabase.auth.signOut();
+  const url = new URL(req.url);
+  return NextResponse.redirect(new URL('/', url.origin));
+}
