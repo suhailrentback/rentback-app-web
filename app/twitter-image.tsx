d9e35064fa61f455f496f44b021d546f1264beb9
suhailrentback <suhail@rentback.app>
2025-10-13 13:57:05 +0200

    Update twitter-image.tsx

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,12 +1,3 @@
-// app/twitter-image.tsx  <-- add this in BOTH repos (web + admin)
-import { ImageResponse } from "next/og";
-export const runtime = "edge";
-export const size = { width: 1200, height: 630 };
-export const contentType = "image/png";
-
-// Reuse the same art as opengraph-image
-export default function TwitterImage() {
-  return fetch(new URL("./opengraph-image", import.meta.url))
-    .then((res) => res.arrayBuffer())
-    .then((buf) => new ImageResponse(buf as any, size));
-}
+// WEB /app/twitter-image.tsx
+export { size, contentType } from "./opengraph-image";
+export { default as default } from "./opengraph-image";
