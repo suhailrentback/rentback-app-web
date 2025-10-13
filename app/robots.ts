724538c61ec7fb5ceb5357b0d4cfa53166ea0155
suhailrentback <suhail@rentback.app>
2025-10-13 13:58:33 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,10 @@
+// WEB /app/robots.ts
+import type { MetadataRoute } from "next";
 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://www.rentback.app/sitemap.xml",
+    host: "https://www.rentback.app",
+  };
+}
