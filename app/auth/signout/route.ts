409263cba53b131df225e835ed5ae1f40d78d6ae
suhailrentback <suhail@rentback.app>
2025-10-13 17:36:56 +0200

    Update route.ts

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -1,10 +1,11 @@
-// app/auth/signout/route.ts
+// WEB: app/auth/sign-out/route.ts
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { supabaseServer } from '@/lib/supabase/server';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 
 export async function POST(request: Request) {
-  const supabase = supabaseServer();
+  const supabase = createRouteHandlerClient({ cookies });
   await supabase.auth.signOut();
-  const url = new URL('/', request.url);
-  return NextResponse.redirect(url);
+  const url = new URL(request.url);
+  return NextResponse.redirect(new URL('/', url.origin));
 }
