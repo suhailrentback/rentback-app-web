dafb15f83dde9a2cac6c37871fff7754b140a432
suhailrentback <suhail@rentback.app>
2025-10-14 16:08:40 +0200

    Update index.ts

--- lib/supabase/index.ts
+++ lib/supabase/index.ts
@@ -1,2 +1,8 @@
 // lib/supabase/index.ts
-export * from "./server";
+export {
+  createServerSupabase,
+  createRouteSupabase,
+  supabaseServer,
+  supabaseRoute,
+} from "./server";
+export { createBrowserSupabase, supabaseClient } from "./client";
